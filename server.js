7c368d6b3a043c10a6ff834d8e5a6b4508351b0d
DannyBoi793 <daniel.garcia@my.simpson.edu>
2024-11-12 14:52:12 -0600

    updated code.

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
 
-const clientId = "your_client_id";
-const clientSecret = "your_client_secret";
-const redirectUri = "http://127.0.0.1:5500/callback.html";
+app.use(express.static("assets"));
+
+const clientId = "401d63283cf74348b61eb45d0531a011";
+const clientSecret = "7bb43d21dd3f43f0abca372dcd1a72f4";
+const redirectUri = "http://127.0.0.1:5500/assets/pages/callback.html";
 
 app.get("/getSpotifyToken", async (req, res) => {
     const code = req.query.code;
@@ -22,9 +24,15 @@ app.get("/getSpotifyToken", async (req, res) => {
     });
 
     const data = await response.json();
-    res.json(data); // Return the token data to the client
+    console.log("Spotify token response data:", data); // Log the response data
+
+        if (response.ok) {
+            res.json(data); // Send the token data to the client
+        } else {
+            res.status(response.status).json(data); // Send error details to the client
+        }
 });
 
 app.listen(5500, () => {
-    console.log("Server running on http://localhost:5500");
+    console.log("Server running on http://127.0.0.1:5500");
 });
